909b29eb977059a2a969c2c0a49d034614b75a26
brito7547 <rebeca.brito18.08@gmail.com>
2026-06-09 18:16:02 -0300

    Update asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "horario-v1";
-const ASSETS = ["./index.html", "./manifest.json"];
+const ASSETS = ["/Escolar/index.html", "/Escolar/manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
